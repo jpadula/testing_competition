6ebff32c53f9ed8439542d81c94d6473b2e2e380
Jorge Padula <jorgeraulpadula@gmail.com>
2015-08-27 19:30:01 -0300

    cleaning code

--- public/modules/users/users.client.module.js
+++ public/modules/users/users.client.module.js
@@ -2,3 +2,29 @@
 
 // Use Applicaion configuration module to register a new module
 ApplicationConfiguration.registerModule('users');
+
+function hasRol(rol,roles) {
+	var result = false;
+	for (var i = roles.length - 1; i >= 0; i--) {
+		if (roles[i]===rol){
+			result = true;
+			break;
+		}
+	};
+	return result;
+}
+
+angular.module('users').run(function (Authentication, $q, Permission) {
+	console.log("D");
+	var roles = Authentication.user.roles;
+	//window.location.reload();
+	Permission
+	//for admin
+    .defineRole('user', function (stateParams) {
+		return hasRol("user",roles);
+	})
+    //for admin
+    .defineRole('admin', function (stateParams) {
+		return hasRol("admin",roles);
+	});
+});
