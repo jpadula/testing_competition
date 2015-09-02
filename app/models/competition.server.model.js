e5d9eb4fc2797e63e4f4735d3e1b319616e67c24
Jorge Padula <jorgeraulpadula@gmail.com>
2015-09-02 16:15:09 -0300

    default POINTS in Competition Model

--- app/models/competition.server.model.js
+++ app/models/competition.server.model.js
@@ -30,6 +30,24 @@ var CompetitionSchema = new Schema({
 	user: {
 		type: Schema.ObjectId,
 		ref: 'User'
+	},
+	POINTS:{
+		FIRST_BUG_IN_CLASS_C: {
+			type:Number,
+			default: 10
+		},
+		NOT_FIRST_BUG_IN_CLASS_C_BUT_YES_IN_ROUTINE_R: {
+			type:Number,
+			default: 5
+		},
+		NOT_FIRST_BUG_IN_CLASS_C_AND_NOT_FIRST_IN_ROUTINE_R: {
+			type:Number,
+			default: 3
+		},
+		PERSON_WHO_SUBMITTED_AN_ACCEPTED_BUG: {
+			type:Number,
+			default: 2
+		},
 	}
 });
 
