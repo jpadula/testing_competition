af55dd9a11e931236e1d3155fc450660a29ba068
Jorge Padula <jorgeraulpadula@gmail.com>
2015-11-23 16:41:43 +0100

    Issue #31

--- public/modules/countries/config/countries.client.config.js
+++ public/modules/countries/config/countries.client.config.js
@@ -3,9 +3,11 @@
 // Configuring the Countries module
 angular.module('countries').run(['Menus',
 	function(Menus) {
+		/*
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Countries', 'countries', 'dropdown', '/countries(/create)?',true,["admin"]);
 		Menus.addSubMenuItem('topbar', 'countries', 'List Countries', 'countries');
 		Menus.addSubMenuItem('topbar', 'countries', 'New Country', 'countries/create');
+		*/
 	}
 ]);
