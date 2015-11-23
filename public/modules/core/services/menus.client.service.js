700471054ecacfdd551ff34b9f822fb19a944be5
Jorge Padula <jorgeraulpadula@gmail.com>
2015-11-23 18:59:02 +0100

    ng-confirm alert

--- public/modules/core/services/menus.client.service.js
+++ public/modules/core/services/menus.client.service.js
@@ -1,5 +1,18 @@
 'use strict';
-
+angular.module('core').directive('ngConfirmClick', [
+	function(){
+	    return {
+	        link: function (scope, element, attr) {
+	            var msg = attr.ngConfirmClick || "Are you sure?";
+	            var clickAction = attr.confirmedClick;
+	            element.bind('click',function (event) {
+	                if ( window.confirm(msg) ) {
+	                    scope.$eval(clickAction)
+	                }
+	            });
+	        }
+	    };
+ }]);
 //Menu service used for managing  menus
 angular.module('core').service('Menus', [
 
