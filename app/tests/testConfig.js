b2bbca1d5a72b1f565a8ff0bdcb168db501ac1ec
Martin Nordio <martin.nordio@inf.ethz.ch>
2015-11-24 14:52:14 +0100

    Starting to setup the testing

---
+++ app/tests/testConfig.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Martin on 24/11/15.
+ */
+'use strict';
+
+//var path = require('path');
+
+// All configurations will extend these options
+// ============================================
+module.exports = {
+
+  timeOut: 50000
+
+};
