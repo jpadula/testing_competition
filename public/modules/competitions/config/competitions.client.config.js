df976099cb34cbcdf4f3ba1aa767dad5a5e68aa4
Jorge Padula <jorgeraulpadula@gmail.com>
2015-11-27 19:59:41 +0100

    quick fix

--- public/modules/competitions/config/competitions.client.config.js
+++ public/modules/competitions/config/competitions.client.config.js
@@ -4,17 +4,17 @@
 angular.module('competitions').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Admin', 'admin', 'dropdown',true,["admin"]);
-		Menus.addSubMenuItem('topbar', 'admin', 'Show Competitions', 'competitions');
-		Menus.addSubMenuItem('topbar', 'admin', 'New Competition', 'competitions/create');
-				
-		Menus.addSubMenuItem('topbar', 'admin', 'Show Groups', 'groups');
-		Menus.addSubMenuItem('topbar', 'admin', 'New Group', 'groups/create');
+		Menus.addMenuItem('topbar', 'Admin', 'competitions', 'dropdown','',true,["admin"]);
+		Menus.addSubMenuItem('topbar', 'competitions', 'Show Competitions', 'competitions');
+		Menus.addSubMenuItem('topbar', 'competitions', 'New Competition', 'competitions/create');
+
+		Menus.addSubMenuItem('topbar', 'competitions', 'Show Groups', 'groups');
+		Menus.addSubMenuItem('topbar', 'competitions', 'New Group', 'groups/create');
 		
-		Menus.addSubMenuItem('topbar', 'admin', 'Logs', 'logs');
+		Menus.addSubMenuItem('topbar', 'competitions', 'Logs', 'logs');
 		
 		// My Competitions for User
-		Menus.addMenuItem('topbar', 'Competitions', 'my_competitions', 'dropdown', '',true,["user"]);
+		Menus.addMenuItem('topbar', 'Competitions', 'my_competitions', 'dropdown','',true,["user"]);
 		Menus.addSubMenuItem('topbar', 'my_competitions', 'Show Competitions', 'my_competitions');
 
 	}
